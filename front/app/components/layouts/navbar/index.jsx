ed175a66640f069c944511dcd70fc90cef85373a
Alexander Krasnoschekov <akrasnoschekov@gmail.com>
2016-11-18 21:45:17 +0300

    shows logout option only when logged in

--- front/app/components/layouts/navbar/index.jsx
+++ front/app/components/layouts/navbar/index.jsx
@@ -4,8 +4,8 @@ import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/lib/Navbar';
 import Nav from 'react-bootstrap/lib/Nav';
 import NavItem from 'react-bootstrap/lib/NavItem';
-import {Link} from 'react-router';
 import { LinkContainer } from 'react-router-bootstrap';
+import auth from '_app/libs/auth';
 
 const NavbarComponent = class NavbarComponent extends Component {
   render() {
@@ -17,7 +17,7 @@ const NavbarComponent = class NavbarComponent extends Component {
         <Nav>
           <LinkContainer to='/records'><NavItem>Records</NavItem></LinkContainer>
           <LinkContainer to='/about'><NavItem>About</NavItem></LinkContainer>
-          <LinkContainer to='/logout'><NavItem>Logout</NavItem></LinkContainer>
+          {auth.loggedIn() ? (<LinkContainer to='/logout'><NavItem>Logout</NavItem></LinkContainer>) : null}
         </Nav>
       </Navbar>
     );
