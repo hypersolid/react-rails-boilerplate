153e3ef68bb1f6049c62b393031231e35262433a
Alexander Krasnoschekov <akrasnoschekov@gmail.com>
2016-11-18 11:51:58 +0300

    adds records#delete spec

--- api/spec/controllers/v1/records_controller_spec.rb
+++ api/spec/controllers/v1/records_controller_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe V1::RecordsController, type: :controller do
       expect_status(200)
     end
     it 'returns one record' do
-      record = create(:record)
+      create(:record)
       get :index
       expect_json_types(records: :array)
       expect_json_sizes(records: 1)
@@ -25,31 +25,47 @@ RSpec.describe V1::RecordsController, type: :controller do
     end
   end
 
-  describe 'PUT index' do
+  describe 'update record' do
     let(:record) { create(:record) }
     it 'responds with correct status code' do
       new_title = Faker::Name.name
-      put :update, params: {id: record.id, record: {title: new_title}}
+      put :update, params: { id: record.id, record: { title: new_title } }
       expect_status(200)
     end
     it 'updates record' do
       new_title = Faker::Name.name
-      put :update, params: {id: record.id, record: {title: new_title}}
+      put :update, params: { id: record.id, record: { title: new_title } }
       expect(record.reload.title).to eq(new_title)
+      expect_json('title', record.title)
     end
   end
 
-  describe 'POST index' do
+  describe 'create record' do
     it 'responds with correct status code' do
-      post :create, params: {record: build(:record).attributes}
+      post :create, params: { record: build(:record).attributes }
       expect_status(201)
     end
     it 'creates record' do
-      expect{
-        post :create, params: {record: build(:record).attributes}
-      }.to change{
+      expect do
+        post :create, params: { record: build(:record).attributes }
+      end.to change{
         Record.count
       }.by(1)
+    end
+  end
+
+  describe 'destroy record' do
+    it 'responds with correct status code' do
+      delete :destroy, params: { id: create(:record).id }
+      expect_status(204)
+    end
+    it 'creates record' do
+      record = create(:record)
+      expect do
+        delete :destroy, params: { id: record.id }
+      end.to change{
+        Record.count
+      }.by(-1)
     end
   end
 end
---
+++ front/README.md
@@ -0,0 +1,7 @@
+swagger
+specs for delete
+specs for react
+uml
+https://refactoring.guru/
+http://cqrs.nu/tutorial/cs/01-design
+http://butunclebob.com/ArticleS.UncleBob.PrinciplesOfOod
